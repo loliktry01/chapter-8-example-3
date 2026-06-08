5df42654d0740536a6deb46eb91e1bfe1ac5dd32
loliktry <liliek.triyono@polines.ac.id>
2026-06-09 00:55:17 +0700

    coba lagin lagi

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -16,11 +16,11 @@
 set -u # or set -o nounset
 # : "$CONTAINER_REGISTRY"
 # : "$VERSION"
-: "$REGISTRY_UN"
-: "$REGISTRY_PW"
+# : "$REGISTRY_UN"
+# : "$REGISTRY_PW"
 
 # echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
-echo $REGISTRY_PW | docker login loliktrykps --username $REGISTRY_UN --password-stdin
+docker login loliktrykps -u loliktrykps --password-stdin <<< "kliwonan@123"
 # docker push $CONTAINER_REGISTRY/video-streaming:$VERSION
 
 docker push loliktrykps/video-streaming:v2
