234c67d3c0ca2ba0b8a18e7286e344819a454030
loliktry <liliek.triyono@polines.ac.id>
2026-06-09 00:41:05 +0700

    Fokus ke PUSH dl

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -14,11 +14,12 @@
 #
 
 set -u # or set -o nounset
-: "$CONTAINER_REGISTRY"
-: "$VERSION"
+# : "$CONTAINER_REGISTRY"
+# : "$VERSION"
 : "$REGISTRY_UN"
 : "$REGISTRY_PW"
 
-echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
-docker push $CONTAINER_REGISTRY/video-streaming:$VERSION
-# docker push loliktrykps/video-streaming:v2
+# echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
+echo $REGISTRY_PW | docker login loliktrykps --username $REGISTRY_UN --password-stdin
+# docker push $CONTAINER_REGISTRY/video-streaming:$VERSION
+docker push loliktrykps/video-streaming:v2
