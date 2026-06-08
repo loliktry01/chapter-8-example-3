1ddb2de8bb1a4d36f0a500c4633c16da7b0ea2ad
loliktry <liliek.triyono@polines.ac.id>
2026-06-09 01:12:48 +0700

    coba lagi PT

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -16,11 +16,11 @@
 set -u # or set -o nounset
 # : "$CONTAINER_REGISTRY"
 # : "$VERSION"
-# : "$REGISTRY_UN"
-# : "$REGISTRY_PW"
+: "$REGISTRY_UN"
+: "$REGISTRY_PW"
 
-# echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
-docker login loliktrykps -u loliktrykps --password-stdin <<< "dckr_pat_k-VpicKCinyBRMnlfKOaHG4O600"
+echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
+# docker login loliktrykps -u loliktrykps --password-stdin <<< "dckr_pat_k-VpicKCinyBRMnlfKOaHG4O600"
 # docker push $CONTAINER_REGISTRY/video-streaming:$VERSION
 
 docker push loliktrykps/video-streaming:v2
